794ef49e591e8a9f70b2cc1ed0c1de7bd09915f1
Hanchen Liu <772185085@qq.com>
2017-10-13 17:51:16 -0700

    Finished Product of calculator app

---
+++ Assignment 2 Calculator App Extended/src/main/java/calculator/Calculator.java
@@ -0,0 +1,68 @@
+package calculator;
+
+/*
+ * Hanchen (Zero) Liu
+ * 10/13/2017
+ * Calculator.java
+ *
+ * This class handles all the calculations
+ */
+
+/**
+ * This class handles all the calculations
+ *
+ * @author Hanchen (Zero) Liu
+ * @version 2.0
+ */
+public class Calculator
+{
+    /**
+     * This method handles normal calculations like addition, subtraction, multiplication, and division
+     *
+     * @param operand1 the operand before operator
+     * @param operand2 the operand after operator
+     * @param operator the operator
+     * @return the calculation result
+     */
+    public static String operation(String operand1, String operand2, String operator)
+    {
+        /*
+         * use a switch statement to check the operator, and do the corresponding calculation
+         */
+        switch (operator)
+        {
+            case "+": return Double.toString(Double.parseDouble(operand1) + Double.parseDouble(operand2));
+            case "-": return Double.toString(Double.parseDouble(operand1) - Double.parseDouble(operand2));
+            case "*": return Double.toString(Double.parseDouble(operand1) * Double.parseDouble(operand2));
+            case "/": if (Double.parseDouble(operand2) == 0) return "False Value";
+                return Double.toString(Double.parseDouble(operand1) / Double.parseDouble(operand2));
+        }
+
+        //throws an exception for handling invalid operators
+        throw new IllegalArgumentException("Invalid Operator!");
+    }
+
+    /**
+     * This method handles special calculations like square, square root, sin, cos, and tan
+     *
+     * @param operand the string before special operator
+     * @param specialOperator the special operator
+     * @return the calculation result
+     */
+    public static String specialOperation(String operand, String specialOperator)
+    {
+        /*
+         * use a switch statement to check the operator, and do the corresponding calculation
+         */
+        switch (specialOperator)
+        {
+            case "x^2": return Double.toString(Math.pow(Double.parseDouble(operand), 2));
+            case "√(x)": return Double.toString(Math.sqrt(Double.parseDouble(operand)));
+            case "Sin(x)": return Double.toString(Math.sin(Math.toRadians(Double.parseDouble(operand))));
+            case "Cos(x)": return Double.toString(Math.cos(Math.toRadians(Double.parseDouble(operand))));
+            case "Tan(x)": return Double.toString(Math.tan(Math.toRadians(Double.parseDouble(operand))));
+        }
+        //throws an exception for handling invalid operators
+        throw new IllegalArgumentException("Invalid Operator!");
+    }
+}
---
+++ Assignment 2 Calculator App Extended/src/main/java/ui/CalculatorUI.java
@@ -0,0 +1,317 @@
+package ui;
+
+import calculator.Calculator;
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.*;
+import javafx.stage.Stage;
+
+import java.util.ArrayList;
+
+/*
+ * Hanchen (Zero) Liu
+ * 10/13/2017
+ * CalculatorUI.java
+ *
+ * This class creates an calculator ui
+ */
+
+ /**
+ * This class creates and calculator ui
+ *
+ * @author Hanchen (Zero) Liu
+ * @version 2.0
+ */
+
+public class CalculatorUI extends Application
+{
+    //use an array to store number buttons from 1 - 9
+    private static final String[] NUMBER_BUTTONS = new String[]{"7", "8", "9", "4", "5", "6", "1", "2", "3"};
+
+    //use an array to store the operators
+    private static final String[] OPERATORS = new String[]{"+", "-", "*", "/"};
+
+    //use an array to store the special operators
+    private static final String[] SPECIAL_OPERATORS = new String[]{"x^2", "√(x)", "Sin(x)", "Cos(x)", "Tan(x)"};
+
+    //set the number of columns
+    private static final int COLUMN_COUNT = 5;
+    //set the number of rows
+    private static final int ROW_COUNT = 5;
+
+    //create some fields to store information
+    private String operand1 = "";
+    private String operand2 = "";
+    private String operator = "";
+    private ArrayList<String> results = new ArrayList<>();
+    private int resultsIndex;
+
+    /**
+     * The main entry point for all JavaFX applications.
+     * The start method is called after the init method has returned,
+     * and after the system is ready for the application to begin running.
+     *
+     * @param stage the primary stage for this application,
+     *             onto which the application scene can be set.
+     *             The primary stage will be embedded in the
+     *              browser if the application was launched as an applet.
+     *              Applications may create other stages, if needed,
+     *              but they will not be primary stages and will not be embedded in the browser
+     *
+     * @throws Exception
+     */
+    @Override
+    public void start(Stage stage) throws Exception
+    {
+        //set the title for the stage
+        stage.setTitle("Calculator");
+
+        //set the scene for the stage
+        stage.setScene(createCalculator());
+
+        //show the stage
+        stage.show();
+    }
+
+    /*
+     * This method creates the calculator ui
+     */
+    private Scene createCalculator()
+    {
+        //assemble our controls in a grid
+        GridPane gridPane = new GridPane();
+
+        //create an output box
+        HBox outputBox = new HBox();
+        Label output = new Label("0");
+
+        outputBox.getChildren().add(output);
+
+        //add the display box to the grid pane
+        gridPane.add(outputBox, 0, 0, 5, 1);
+
+        /*
+         * create an all clear button which will not only reset the output text,
+         * but also clears all the operands, operator, and the history results
+         */
+        Button allClearButton = new Button("CE");
+        allClearButton.setOnAction(new EventHandler<ActionEvent>()
+        {
+            @Override
+            public void handle(ActionEvent event)
+            {
+                output.setText("0");
+                operand1 = "";
+                operand2 = "";
+                operator = "";
+                results.clear();
+            }
+        });
+
+        gridPane.add(allClearButton, 0, 1);
+
+        //create a clear button that resets the output text
+        Button clearButton = new Button("C");
+        clearButton.setOnAction(new EventHandler<ActionEvent>()
+        {
+            @Override
+            public void handle(ActionEvent event)
+            {
+                output.setText("0");
+            }
+        });
+
+        gridPane.add(clearButton, 1, 1);
+
+        /*
+         * create a show stored result button that will show one
+         * previous result every time the button is clicked
+         */
+        Button showStoredResultButton = new Button("ANS");
+
+        //set a index that is used to go through the results list
+        if (!results.isEmpty()) resultsIndex = results.size()-1;
+        else resultsIndex = 0;
+        showStoredResultButton.setOnAction(new EventHandler<ActionEvent>()
+        {
+            @Override
+            public void handle(ActionEvent event)
+            {
+                if (results.isEmpty())
+                {
+                    output.setText("0");
+                }
+
+                /*
+                 * when all the previous results are gone through,
+                 * loop back to the last/latest result
+                 */
+                else
+                {
+                    output.setText(results.get(resultsIndex));
+                    if (resultsIndex > 0)
+                    {
+                        resultsIndex--;
+                    }
+                    else resultsIndex = results.size() - 1;
+                }
+            }
+        });
+
+        gridPane.add(showStoredResultButton, 2, 1);
+
+        //create a positive negative button that will change the sign of the present number
+        Button positiveNegativeButton = new Button("+/-");
+        positiveNegativeButton.setOnAction(new EventHandler<ActionEvent>()
+        {
+            @Override
+            public void handle(ActionEvent event)
+            {
+                if (output.getText().startsWith("-")) output.setText(output.getText().replace("-", ""));
+                else output.setText("-" + output.getText());
+            }
+        });
+
+        gridPane.add(positiveNegativeButton, 3, 1);
+
+        //set an int to represent the array's index, starts from 0
+        int buttonIndex = 0;
+
+        //use a for loop to create number buttons
+        for (int i = 2; i <= ROW_COUNT-1; i++)
+        {
+            for (int j = 0; j < COLUMN_COUNT-2; j++)
+            {
+                final Button tempButton = new Button(""+ NUMBER_BUTTONS[buttonIndex]+"");
+                tempButton.setOnAction(new EventHandler<ActionEvent>()
+                {
+                    @Override
+                    public void handle(ActionEvent event)
+                    {
+                        if (output.getText().equals("0") || output.getText().equals("-0")
+                                || (!results.isEmpty() && results.contains(output.getText()))
+                                || output.getText().equals("False Value"))
+                        {
+                            output.setText(tempButton.getText());
+                        }
+
+                        else output.setText(output.getText() + tempButton.getText());
+                    }
+                });
+
+                gridPane.add(tempButton, j, i);
+                buttonIndex++;
+            }
+        }
+
+        //use for loop to create operator buttons
+        for (int k = 0; k < OPERATORS.length; k++)
+        {
+            final Button tempButton = new Button(""+ OPERATORS[k]+"");
+
+            tempButton.setOnAction(new EventHandler<ActionEvent>()
+            {
+                @Override
+                public void handle(ActionEvent event)
+                {
+                    if (output.getText().equals("False Value")) output.setText("False Value");
+                    else
+                    {
+                        operand1 = output.getText();
+                        operator = tempButton.getText();
+                        output.setText("0");
+                    }
+                }
+            });
+
+            gridPane.add(tempButton, COLUMN_COUNT-2, k + 2);
+        }
+
+        //use for loop to create special operator buttons
+        for (int l = 0; l < SPECIAL_OPERATORS.length; l++)
+        {
+            final Button tempButton = new Button(""+ SPECIAL_OPERATORS[l]+"");
+
+            tempButton.setOnAction(new EventHandler<ActionEvent>()
+            {
+                @Override
+                public void handle(ActionEvent event)
+                {
+                    if (output.getText().equals("False Value")) output.setText("False Value");
+                    else
+                    {
+                        //add the result to the result list
+                        results.add(Calculator.specialOperation(output.getText(), tempButton.getText()).replaceAll("\\.?0*$", ""));
+                        output.setText(results.get(results.size()-1));
+                    }
+                }
+            });
+
+            tempButton.setId("specialOperator");
+            gridPane.add(tempButton, COLUMN_COUNT-1, l + 1);
+        }
+
+        //creates the 0 button
+        Button button0 = new Button("0");
+        button0.setOnAction(new EventHandler<ActionEvent>()
+        {
+            @Override
+            public void handle(ActionEvent event)
+            {
+                if (output.getText().equals("0") || output.getText().equals("False Value")) output.setText(button0.getText());
+                else output.setText(output.getText() + button0.getText());
+            }
+        });
+        gridPane.add(button0, 0, 5);
+
+        //creates an decimal button
+        Button decimalButton = new Button(".");
+
+        decimalButton.setOnAction(new EventHandler<ActionEvent>()
+        {
+            @Override
+            public void handle(ActionEvent event)
+            {
+                if (!output.getText().contains(".") && !output.getText().equals("False Value"))
+                {
+                    output.setText(output.getText() + decimalButton.getText());
+                }
+                else output.setText(output.getText());
+            }
+        });
+
+        gridPane.add(decimalButton, 1, 5);
+
+        //creates the equal button
+        Button equalButton = new Button("=");
+
+        equalButton.setOnAction(new EventHandler<ActionEvent>()
+        {
+            @Override
+            public void handle(ActionEvent event)
+            {
+                if (operator.isEmpty()) output.setText(output.getText());
+                else
+                {
+                    operand2 = output.getText();
+                    results.add(Calculator.operation(operand1, operand2, operator).replaceAll("\\.?0*$", ""));
+                    output.setText(results.get(results.size() - 1));
+                    operator = "";
+                }
+            }
+        });
+
+        gridPane.add(equalButton, 2, 5);
+
+        //add styles to the elements using an external stylesheet
+        Scene mainScene = new Scene(gridPane, 420, 430);
+        mainScene.getStylesheets().add("styles/Styles.css");
+
+        //returns the scene
+        return mainScene;
+    }
+}
